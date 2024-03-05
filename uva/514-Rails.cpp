90132d1f6a0c036972215dd723d29ad3e9f4885c
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-03-04 21:50:55 -0500

    It's not my code, but it's really amazing, 514 uva ended

--- uva/514-Rails.cpp
+++ uva/514-Rails.cpp
@@ -1,41 +1,32 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-int main () {
-  int n, b = 1;
-  while (scanf("%d", &n) != EOF && n != 0) {
-    if(!b) printf("\n");
-    //printf("salto de bloque: %d\n", n);
-    //esto representa cada linea del bloque
-    while (scanf("%d", &b), b != 0) {
-      //cout << "primer número: " << b << '\n';
-      stack<int> s;
-      int turn = 1, sca = n - 1;
-      while (b != turn) {
-        s.push(turn);
-        turn++;
-      }
-      while (sca > 0 && turn <= n) {
-        scanf("%d", &b);
-        sca--;
-        //revisar stack
-        while (!s.empty() && b == s.top()) {
-          s.pop();
-          if (sca) scanf("%d", &b);
-          //cout << "segundo número: " << b << '\n';
-          sca--;
-        }
-        turn++;
-        while (b != turn && turn < n + 1) {
-          turn++;
-        }
-      }
-      scanf("%*[^\n]");
-      if (s.empty())
-        printf("Yes\n");
-      else 
-        printf("No\n");
-    }
-  }
-  return 0;
-}
 
+int main() {
+  int n;
+  while (cin>> n && n) {
+    while (true) {
+      vector <int> v (n);
+      cin >> v[0];
+      if(!v[0]) break;
+      int m = n-1, i=1;
+      while(m--){
+        cin>>v[i++];
+      }
+      stack<int>st;
+      m = 0, i = 1;
+      while(m<n) {
+        if(!st.empty() && st.top() == v[m]) {
+          m++;
+          st.pop();
+        } else {
+          if(i>n) break;
+          st.push(i++);
+        }
+      }
+      if(st.empty()) cout<<"Yes"<<'\n';
+      else cout <<"No" << '\n';
+    }
+    cout << '\n';
+  }
+	return 0;
+}
