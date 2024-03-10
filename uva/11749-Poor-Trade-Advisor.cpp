a1b5fad62e9c8c1cdb836e7ca5619ff68aa99416
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-03-10 18:49:27 -0500

    I finished problem Inception, it was so easy

---
+++ uva/11749-Poor-Trade-Advisor.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main () {
+  int n, m;
+  while (1) {
+    scanf("%d %d", &n, &m);
+    if (n == 0) break;
+    long sumppa = 0;
+    double avr = 0;
+    vector<vector< pair<int, int>>> graph(n);
+    for (int i = 0; i < m; i++) {
+      int v1, v2, w;
+      scanf("%d %d %d", &v1, &v2, &w);
+      //printf("leidos: %d, %d, %d\n", v1, v2, w);
+      v1--; v2--;
+      graph[v1].emplace_back(v2, w);
+      graph[v2].emplace_back(v1, w);
+      sumppa += w;
+    }
+    avr= (double)sumppa / (double)m;
+    //printf("el promedio será : %ld / %d = %f\n", sumppa, m, avr);
+    int cities = 0;
+    for (auto vertex : graph) {
+      long weight = 0;
+      double avrw;
+      for (auto v : vertex) {
+        weight += v.second;
+      }
+      avrw = (double) weight / (double)vertex.size();
+      if (avrw > avr) 
+      {
+        cities++;
+      }
+    }
+    printf("%d\n", cities);
+  }
+  return 0;
+}
+
---
+++ uva/13055-Inception.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main () {
+  string line;
+  stack<string> people;
+  int n; scanf("%d", &n);
+  for (int i = 0; i <= n; i++) {
+    getline(cin, line);
+    switch (line[0]) {
+      case 'S':
+        people.push(line.substr(6));
+        break;
+      case 'T':
+        if (!people.empty())
+          cout << people.top() << '\n';
+        else printf("Not in a dream\n");
+        break;
+      //case 'K':
+      default:
+        if (!people.empty())
+          people.pop();
+        break;
+    }
+  }
+  return 0;
+}
+
