05a68c146371d3d9b05f3f6d5947521e37006b86
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-03-11 22:04:40 -0500

    Waiting for the judging

---
+++ uva/551-Nesting-a-Bunch-of-Brackets.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h> 
+using namespace std;
+char open[] = { '{', '[', '<' };
+char closed[] = { '}', ']', '>' };
+
+bool isOpen(const int& c) {
+  int n = 3;
+  while (n--) {
+    if (open[n] == c) return true;
+  }
+  return false;
+}
+
+bool isClose(const int& c) {
+  int n = 3;
+  while (n--) {
+    if (closed[n] == c) return true;
+  }
+  return false;
+}
+
+int main () {
+  string line;
+  //for (int i = 0; i < 3; i++) {
+  //  cout << open[i] << "->" << (char)(open[i] + 2) << '\n'; 
+  //}
+  
+  while (getline(cin, line)) {
+    stack<char> pila;
+    int count = 0;
+    int pending = 0;
+    int next = 0;
+    for (int i = 0; i < line.size(); i++) {
+      count++;
+      if (isOpen(line[i])) {
+        pila.push(line[i]);
+        continue;
+      }
+      if (isClose(line[i])) {
+        if (!pila.empty() && pila.top() == line[i]-2 ){
+            pila.pop();
+            continue;
+        } else {
+          printf("NO %d\n", count);
+          //cout << "<<" << pila.top() << '\n';
+          return 0;
+        }
+      }
+      if (line[i] == '(') {
+        pending = 1;
+        pila.push('(');
+        continue;
+      }
+      if (line[i] == ')') {
+        if (next) {
+          count--;
+          next = 0;
+          if (!pila.empty() && pila.top() == 'a' ){
+            pila.pop();
+            continue;
+          } else {
+            printf("NO %d\n", count);
+            return 0;
+          }
+        }
+      }
+      if (line[i] == '*') {
+        if (pending) {
+          pending = 0;
+          pila.pop();
+          pila.push('a');
+          count--;
+          continue;
+        }
+        next = 1;
+      }
+    }
+    if (pila.empty()) 
+      printf("YES\n");
+    else 
+    {
+      printf("->NO %d\n", count);
+      //cout << pila.top() << '\n';
+    }
+  }
+  return 0;
+}
+
