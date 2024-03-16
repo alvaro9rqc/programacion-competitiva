59183e62abf52a2f560002ad44f4b2622f905484
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-03-15 21:33:31 -0500

    I wasn't able to solve it

--- uva/11111-Generalized-Matrioshkas.cpp
+++ uva/11111-Generalized-Matrioshkas.cpp
@@ -11,18 +11,11 @@ int main () {
       line.push_back(n);
       if (dumy == '\n') break;
     }
-    //for (int i = 0; i < line.size(); i++) {
-    //  cout << line[i] << "°"; 
-    //}
     stack <pair<int, int>> pila;
     pila.push({line[0], 0});
     int flag = 1;
-    //cout << line[12] << "-> "<< line.size() <<'\n';
     for (int i = 1; i < line.size(); i++) {
-      if (line[i] < 0 /*&& !pila.empty() && line[i] > pila.top().first*/) {
-        if (!pila.empty() && line[i] <= pila.top().first) {
-          flag = 0; break;
-        }
+      if (line[i] < 0 && !pila.empty() && line[i] > pila.top().first) {
         pila.push({line[i], 0});
         continue;
       }
@@ -32,17 +25,11 @@ int main () {
           pila.top().second += line[i];
           if (pila.top().first * -1 <= pila.top().second) {
             flag = 0;
-            //cout << line[i] << "DX\n";
-            //cout << pila.top().first << "°" << pila.top().second << '\n';
             break;
           }
         } 
         continue;
       }
-      //cout << line[i] << "DX\n";
-      //cout << "second\n";
-      //cout << line[i] << " o " << i <<"DX\n";
-      //cout << pila.top().first << "°" << pila.top().second << '\n';
       flag = 0;
       break;
     }
@@ -52,10 +39,8 @@ int main () {
     line.clear();
     if (dumy == '\n') 
     {
-      //cout << "fin\n";
       continue;
     }
-    //cout << '\n';
   }
   return 0;
 }
