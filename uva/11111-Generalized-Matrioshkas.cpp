34be0554029ff4e11c3f73700a7a0ca262181082
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-03 16:34:30 -0500

    I found out that a empty line is a possible case

--- uva/11111-Generalized-Matrioshkas.cpp
+++ uva/11111-Generalized-Matrioshkas.cpp
@@ -3,66 +3,38 @@ using namespace std;
 int main () {
   char dumy;
   int n;
-  vector <int> line;
   while (true) {
+    //scan the elements
+    vector <int> line;
     if (scanf("%d%c", &n, &dumy) == EOF ) break;
     line.push_back(n);
     while (scanf("%d%c", &n, &dumy)) {
       line.push_back(n);
       if (dumy == '\n') break;
     }
-    stack <pair<int, int>> pila;
+    stack <pair<int, int>> st;
+    bool f = true;
+    //operations
     for (int i = 0; i < line.size(); i++) {
-      if (pila.empty()) {
-        pila.push({line[i], 0});
-      } else {
-        if (pila.top().first == line[i] * -1) 
-          pila.pop();
-        else if (abs(pila.top().first) <= abs(line[i])) 
-          break;
-        else {
-          pila.top().second += line[i];
-          if (abs(pila.top().first) <= abs(pila.top().second))
+      if (st.empty() && line[i] < 0) st.push({line[i], 0});
+      else if (!st.empty() && st.top().first == -line[i]) {
+        st.pop();
+        if (!st.empty()) {
+          st.top().second -= line[i];
+          if (st.top().second <= st.top().first) {
+            f = false;
             break;
-          pila.push({line[i], 0});
+          }
         }
+      } else if (!st.empty() && line[i] < 0) {
+        st.push({line[i], 0});
+      } else {
+        f = false;
+        break;
       }
     }
-    if (pila.empty()) {
-      cout << ":-) Matrioshka!\n";
-    } else {
-      printf(":-( Try again.\n");
-    }
-    //stack <pair<int, int>> pila;
-    //pila.push({line[0], 0});
-    //int flag = 1;
-    //for (int i = 1; i < line.size(); i++) {
-    //  if (line[i] < 0 && !pila.empty() && line[i] > pila.top().first) {
-    //    pila.push({line[i], 0});
-    //    continue;
-    //  }
-    //  if (!pila.empty() && line[i] * -1 == pila.top().first) {
-    //    pila.pop();
-    //    if (!pila.empty()) {
-    //      pila.top().second += line[i];
-    //      if (pila.top().first * -1 <= pila.top().second) {
-    //        flag = 0;
-    //        break;
-    //      }
-    //    } 
-    //    continue;
-    //  }
-    //  flag = 0;
-    //  break;
-    //}
-    //if (!pila.empty()) flag = 0;
-    //if (flag) printf(":-) Matrioshka!\n");
-    //else printf(":-( Try again.\n");
-    line.clear();
-    if (dumy == '\n') 
-    {
-      continue;
-    }
+    if (f) cout << ":-) Matrioshka!\n";
+    else cout << ":-( Try again.\n"; 
   }
   return 0;
 }
