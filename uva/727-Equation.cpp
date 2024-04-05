5cc6797b26d509cacfa97f9b6a9361659319530e
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-05 09:23:46 -0500

    I got AC uva 727

--- uva/727-Equation.cpp
+++ uva/727-Equation.cpp
@@ -1,19 +1,5 @@
 #include <bits/stdc++.h> 
 using namespace std;
-int isOperator(const int& c) {
-  switch (c) {
-    case '+':
-      return 1;
-    case '-':
-      return 1;
-    case '*':
-      return 1;
-    case '/':
-      return 1;
-    default:
-      return 0;
-  }
-}
 int main () {
   char c;
   int n; scanf("%d\n", &n);
@@ -54,61 +40,24 @@ int main () {
           pila.pop();
         }
         pila.push(in[i]);
-      }
-    }
-    while (scanf("%c", &c) != EOF){
-      if (c == '\n')
-        break;
-      scanf("%*c");
-      //cout << c << "-";
-      if ( '0' <= c && c <= '9'  ) {
-        ecu.push_back(c);
         continue;
       }
-      if (c == '(') {
-        pila.push(c);
-        continue;
-      }
-      if (c == ')') {
+      if (in[i] == '+' || in[i] == '-') {
         while (!pila.empty() && pila.top() != '(') {
           ecu.push_back(pila.top());
           pila.pop();
         }
-        pila.pop();
+        pila.push(in[i]);
         continue;
       }
-      //only if this is an operator
-      if (c == '*' || c == '/') {
-        while (!pila.empty()) {
-          char aux = pila.top();
-          if (aux != '(' && (aux == '*' || aux == '/')) {
-            ecu.push_back(aux);
-            pila.pop();
-          } else {
-            break;
-          }
-        }
-        pila.push(c);
-        continue;
-      }
-      //if c == + or -
-      while (!pila.empty()) {
-        if (pila.top() != '(') {
-          ecu.push_back(pila.top());
-          pila.pop();
-        } else {
-          break;
-        }
-      }
-      pila.push(c);
     }
+    //the end;
     while (!pila.empty()) {
       ecu.push_back(pila.top());
       pila.pop();
-      cout << ecu << '\n';
     }
-    if (n != 0) printf("\n");
-    cout << '\n';
+    cout << ecu << "\n";
+    if (n) cout << '\n';
   }
   return 0;
 }
