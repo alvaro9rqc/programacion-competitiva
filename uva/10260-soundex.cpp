e0d1df6905843ac2e03b92b3e3746a0c8545ab9f
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-08 21:46:07 -0500

    I added the firt idea to solve 10260

---
+++ uva/10260-soundex.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+//idea: pues haces una función
+//que te tome los arrays y un valor que asignaras a esas posisiones
+//si no, todo lo demás será 0.
+void transf (char a[], int i) {
+  
+}
+int main () {
+   
+  return 0;
+}
+
