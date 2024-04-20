9a3a52daf965aeac74b1886d4a4d5ef9306aba03
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-20 10:04:03 -0500

    I think that I solved It, I am wating for the judge

--- uva/12626-I-❤-Pizza.cpp
+++ uva/12626-I-❤-Pizza.cpp
@@ -8,7 +8,15 @@ int main () {
     getline(cin, line);
     for (char &x : line) 
       alf[x - 'A']++;
+    int m = alf['M' - 'A'] / 1;
+    if (alf['A' - 'A'] / 3 < m) m = alf['A' - 'A'] / 3;
+    //if (alf['M' - 'A'] / 1 < m) m = alf['M' - 'A'] / 1;
+    if (alf['R' - 'A'] / 2 < m) m = alf['R' - 'A'] / 2;
+    if (alf['G' - 'A'] / 1 < m) m = alf['G' - 'A'] / 1;
+    if (alf['I' - 'A'] / 1 < m) m = alf['I' - 'A'] / 1;
+    if (alf['T' - 'A'] / 1 < m) m = alf['T' - 'A'] / 1;
     //right, I need to know how many margarita are there
+    cout << m << '\n';
   }
   return 0;
 }
