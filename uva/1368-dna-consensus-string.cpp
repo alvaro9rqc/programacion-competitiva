910a3a8a61ce3a6e2937ba9985cb1cae63a6572b
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-21 17:34:36 -0500

    I liked this problem, I learned a lot of implementations of c++, 1368 AC

--- uva/1368-dna-consensus-string.cpp
+++ uva/1368-dna-consensus-string.cpp
@@ -4,23 +4,32 @@ int main () {
   int t; scanf("%d", &t); 
   while (t--) {
     int m, n; scanf("%d %d\n", &m, &n);
-    vector < string > v(m);
     vector < unordered_map <char, int> >maps (n, { 
         {'A', 0},
         {'T', 0},
         {'G', 0},
         {'C', 0}
         }); 
-    //maps.assign(n, unordered_map<char, int>() );
-    vector <int> rep(n, 0);
+    string line;
+    string ans(n, 'A');
     for (int i = 0; i < m; i++) {
-      getline(cin, v[i]);
+      getline(cin, line);
       for (int j = 0; j < n; j++) {
-        maps[j][v[i][j]]
-        if ()
+        maps[j][line[j]]++;
+        if ( maps[j][line[j]] >= maps[j][ ans[j] ] ) {
+          if (  maps[j][line[j]] == maps[j][ ans[j] ]  ) {
+            if (line[j] < ans[j]) ans[j] = line[j];
+          } else {
+            ans[j] = line[j];
+          }
+        }
       }
     }
-    //cout << v[m - 1] << '\n'; -- all correct
+    int cons = 0;
+    for (int i = 0; i < n; i++) {
+      cons += m - maps[i][ans[i]];
+    }
+    printf("%s\n%d\n", ans.c_str(), cons);
 
   }
   return 0;
