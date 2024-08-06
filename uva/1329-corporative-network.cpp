5f42dcb34abcc3a952e48eb01e6f0c1cd7584c3a
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-08-05 22:07:14 -0500

    I have some problems with the routes

--- uva/1329-corporative-network.cpp
+++ uva/1329-corporative-network.cpp
@@ -26,18 +26,37 @@ class UnionFind {
 
     void unionSet(int i, int j) {
       if ( isSameSet(i,j) ) return;
-      int x = findSet(i), y = findSet(j);
+      int x = findSet(i), y = findSet(j); //I could cause sombre problems
       if (rank[x] > rank[y]) swap(x, y);
       p[x] = y;
       if (rank[x] == rank[y]) ++rank[y];
       len[y] += len[x] + ( abs(i - j) % 1000 );
     }
 
-}
+};
 
 int main () {
   ios_base::sync_with_stdio(false);
-  int N, I, J; 
+  int T, N, I, J; 
+  char A;
+  string line;
+  cin >> T;
+  while (T--) {
+    cin >> N;
+    UnionFind uf(N);
+    while (getline(cin, line)){
+      if (line[0] == '0') break;
+      istringstream buff(line);
+      cin >> A; 
+      if (A == 'E') {
+        cin >> I;
+        cout << uf.getLen(--I) << '\n';
+      } else {
+        cin >> I >> J;
+        uf.unionSet(--I, --J);
+      }
+    }
+  }
   return 0;
 }
 
