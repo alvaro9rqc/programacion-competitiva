582201584c733771bb2690fd3affd95614261ff5
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-08-11 21:42:07 -0500

    I fixed some bugs, but in some moment, it doesn't work properly, there is some set extra

--- uva/11987-Almost-Union-Find.cpp
+++ uva/11987-Almost-Union-Find.cpp
@@ -45,31 +45,55 @@ class UnionFind {
       if (rank[x] == rank[y]) rank[y]++;
       setSize[y] += setSize[x];
       sum[y] += sum[x];
+      //cout << "sum " << y << " " << x << ": " << sum[y];
     }
 
     void moveToSet(int i, int j) {
       if ( isSameSet(i, j) ) return;
-      setSize[ findSet(i) ]--;
-      sum[findSet(i)] -= i;
+      //setSize[ findSet(i) ]--;
+      //sum[findSet(i)] -= i;
       int lastChild = -1;
-      for ( auto it = childs[i].begin(); it != childs[i].end(); ++it ) {
-        if ( isSameSet(i, *it) ) lastChild = *it;
-        else childs[i].erase(*it);
+      for ( auto it = childs[i].begin(); it != childs[i].end();) {
+        if (isSameSet(i, *it)) {
+          lastChild = *it; ++it;
+        } else it = childs[i].erase(it);
+        //if ( isSameSet(i, *it) ) lastChild = *it;
+        //else childs[i].erase(*it);
       }
       if (p[i] == i && lastChild != -1) {
         for ( auto it = childs[i].begin(); it != childs[i].end(); ++it ) {
           p[*it] = lastChild;
         }
+        setSize[ lastChild ] = --setSize[i];
+        sum[ lastChild ] = sum[i] - i;
       }
       childs[i].clear();
       p[i] = i;
+      rank[i] = 0;
+      sum[i]=i;
+      setSize[i] = 1;
       unionSet(i, j);
     }
-}
+};
 
 int main () {
   ios_base::sync_with_stdio(false);
-
+  int n, m, o, p, q;
+  while (cin >> n >> m) {
+    UnionFind uf(n+1);
+    while (m--) {
+      //cout << "m " << m << '\n'; 
+      cin >> o;
+      if (o == 3) cin >> p;
+      else cin >> p >> q;
+      if (o == 1) uf.unionSet(p , q);
+      else if (o == 2) uf.moveToSet(p, q);
+      else {
+        cout << uf.sizeOfSet(p) << " " << uf.sumSet(p) << '\n';
+      }
+      //cout << " o " << o << '\n';
+    }
+  }
   return 0;
 }
 
