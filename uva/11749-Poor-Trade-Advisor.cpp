b22f0c14fa58b39eea864270c515cdea3d9c334c
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-09-04 16:39:02 -0500

    After so much time, I could do it, jeeeeee, 11749 AG

--- uva/11749-Poor-Trade-Advisor.cpp
+++ uva/11749-Poor-Trade-Advisor.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h> 
 using namespace std;
-int main () {
-  int n, m;
-  while (1) {
-    scanf("%d %d", &n, &m);
-    if (n == 0 && m == 0) break;
-    long sumppa = 0;
-    double avr = 0;
-    vector<vector< pair<int, int>>> graph(n);
-    for (int i = 0; i < m; i++) {
-      int v1, v2, w;
-      scanf("%d %d %d", &v1, &v2, &w);
-      //printf("leidos: %d, %d, %d\n", v1, v2, w);
-      v1--; v2--;
-      graph[v1].emplace_back(v2, w);
-      graph[v2].emplace_back(v1, w);
-      sumppa += w;
-    }
-    avr= (double)sumppa / (double)m;
-    //printf("el promedio será : %ld / %d = %f\n", sumppa, m, avr);
-    int cities = 0;
-    for (auto vertex : graph) {
-      long weight = 0;
-      double avrw;
-      for (auto v : vertex) {
-        weight += v.second;
-      }
-      avrw = (double) weight / (double)vertex.size();
-      if (avrw > avr) 
-      {
-        cities++;
-      }
-    }
-    printf("%d\n", cities);
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+int dfs(vector<vi>& g, vector<bool>& vis, int u) {
+  int c = 1;
+  vis[u] = true;
+  for (auto & v : g[u]){
+    if (! vis[v])
+      c+=dfs(g, vis, v);
   }
-  return 0;
+  return c;
+}
+int main () {
+  while (1){
+    int n, m; cin >> n >> m;
+    if (n==0&&m==0) break;
+    map<int, vii> EL;
+    //vector<vii> EL(m, vii());
+    int max = INT_MIN;
+    while (m--) {
+      int v, u, w; cin >> v >> u>>w;
+      --u;--v;
+      if (w > max) max = w;
+      EL[w];
+      EL[w].emplace_back(v, u);
+      EL[w].emplace_back(u, v);
+    }
+    vector<vi> g(n, vi());
+    for (auto &[u, v]:EL[max]) 
+    {
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    vector<bool> vis(g.size(), false);
+    int numCC =0;
+    for (int u=0; u < g.size(); ++u) 
+      if (!vis[u]) {
+        int temp = dfs(g, vis, u);
+        if (temp > numCC) numCC = temp;
+      }
+    cout << numCC << '\n';
+    //cout << n << ' '<< max<< '\n';
+  }
 }
 
