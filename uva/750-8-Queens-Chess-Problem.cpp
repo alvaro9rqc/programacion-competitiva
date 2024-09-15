3547cc9ce49555046fec6fafb293c519be9b1b07
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-09-14 21:03:28 -0500

    It was my first backtraking, 750 AC

--- uva/750-8-Queens-Chess-Problem.cpp
+++ uva/750-8-Queens-Chess-Problem.cpp
@@ -6,26 +6,57 @@ typedef vector<ii> vii;
 
 int row[8], a,b,lc;
 
-backtraking(int c) {
+void p() {
+  printf("%2d      %d %d %d %d %d %d %d %d\n",
+      ++lc,
+      1+row[0],
+      1+row[1],
+      1+row[2],
+      1+row[3],
+      1+row[4],
+      1+row[5],
+      1+row[6],
+      1+row[7]
+      );
+}
+
+bool bounded(int c) {
+  for (int i = 0; i < c; ++i) 
+    if (
+        (row[i] == row[c]) || 
+        (abs(i-c) == abs(row[i] - row[c]))
+       )
+      return false;
+  return true;
+}
+
+void backtraking(int c) {
   if (c == 7) {
-    for (int i = 0; i<7;++i) {
+    for (int i = 0; i<8;++i) {
       row[c] = i;
-      if (bounded() && row[b] == a)
-        printf("1 1 5 8 6 3 7 2 4");
+      if (bounded(c) && row[b] == a)
+        p();
+      //else if (b == a && a == 7) printf("%d %d\n", row[b], row[0]);
     }
+    return;
   }
-  for (int i = 0; i < 6)
+  for (int i = 0; i < 8; ++i) {
+    row[c] = i;
+    if (bounded(c)) backtraking(c+1);
+  }
 }
 
 int main () {
   ios_base::sync_with_stdio(false);
   int t; scanf("%d", &t);
   while (t--) {
-    scanf("%d %d", &a, &b);
-    menset(row, 0,sizeof row);
+    scanf("%d %d", &a, &b); --a;--b;
+    memset(row, 0,sizeof row);
+    lc=0;
+    printf("SOLN       COLUMN\n");
+    printf(" #      1 2 3 4 5 6 7 8\n\n");
     backtraking(0);
+    if (t) printf("\n");
   }
   return 0;
 }
-
-
