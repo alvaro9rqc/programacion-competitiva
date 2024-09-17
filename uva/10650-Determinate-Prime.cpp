28028daaaefd0c0b04db7671ee5c08182473e004
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-09-17 18:15:32 -0500

    Ok, it's harder, I need more time

---
+++ uva/10650-Determinate-Prime.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  bitset<32001> bs;
+  bs.set();
+  vi p;
+  map<int, int> m;
+  bs[0] = bs[1] = 0;
+  for (long i = 2; i < 32001; ++i)
+    if ( bs[i] ) {
+      for (long j = i*i; j < 32001; j+=i) bs[j] = 0;
+      p.emplace_back(i);
+      m[i] = 0;
+    }
+  for(int i = 0; i+1 < p.size(); ++i) {
+    if (! m[ p[i] ]) {
+      int dif = p[i+1] - p[i];
+      int c = 1;
+      for (int j = i+2; j < p.size(); ++j)
+        if (p[j] - p[j-1] == dif) 
+          c++;
+        else break;
+      if (c > 1){
+        for (int j = i+1; j <= i + c; ++j)
+          m[ p[i+1] ] = -1;
+        m[ p[i] ] = c;
+      }
+    }
+  }
+  int x, y;
+  while (cin >> x >> y) {
+    if (x == 0 && y == 0) break;
+    if (x >y) swap(x,y);
+    auto it = lower_bound(p.begin(), p.end(), x);
+    if (it  == p.end()) continue;
+    for (it; *it < y; it++) {
+      //cout << "-- " << x << y << '\n';
+      if ( m[ *it ] > 0 && m[*it] + *it < y ) {
+        int rep = m [ *it ];
+        for (int i = 0; i < rep; ++i)
+        {
+          cout << *it << ' ';
+          it++;
+        }
+        cout << *it << '\n';
+      }
+    }
+  }
+
+  return 0;
+}
+
+
