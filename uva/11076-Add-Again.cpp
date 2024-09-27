01101ce2f84cf08fff5f1884c980ca9a00f95735
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-09-27 16:56:36 -0500

    It was hard, but I loved it, 11076 AC

--- uva/11076-Add-Again.cpp
+++ uva/11076-Add-Again.cpp
@@ -6,22 +6,33 @@ typedef vector<ii> vii;
 
 int main () {
   ios_base::sync_with_stdio(false);
-  vector<long> ans(13); 
-  vector<long> fac(13); 
+  vector<unsigned long> ans(13); 
+  vector<unsigned long> fac(13); 
   fac[0] = 1;
-  long h = 0;
-  for (long i = 1; i < ans.size(); ++i) 
+  ans[0] = 0;
+  for (unsigned long i = 1; i < ans.size(); ++i) 
   {
     fac[i] = fac[i-1]*i;
-    h = h*10 + 1;
-    ans[i] = h * fac[i-1];
+    ans[i] = ans[i-1]*10+1;
   }
-  long n;
+  unsigned long n;
   while ( cin >> n ) {
     if (n == 0) break;
-    vector<long> v(n);
-    for (int i = 0; i < n; ++i) cin >> v[i];
-    cout << accumulate(v.begin(), v.end(), 0L) * ans[n] << '\n';
+    unsigned long s = 0;
+    unsigned long x;
+    unordered_map<unsigned long, int> m;
+    for (int i = 0; i < n; ++i) {
+      cin >> x;
+      if (m.find(x) == m.end()) m[x] = 1;
+      else m[x]++;
+      s += x;
+    }
+    if (m.size() == 1) cout << x * ans[n] << '\n';
+    else {
+      unsigned long a = s*fac[n-1];
+      for (auto& [ k, v ] : m) a /= fac[v];
+      cout << a * ans [n] << '\n';
+    }
   }
   return 0;
 }
