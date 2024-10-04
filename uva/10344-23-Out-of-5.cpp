5834a5baa2a68e482813e5da5cbc7af10803b676
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-10-03 19:18:29 -0500

    I got a WA

---
+++ uva/10344-23-Out-of-5.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+vi v(5);
+
+bool bactraking(int n, int i) {
+  if (i == 0) return (n == v[i]);
+  bool f = false;
+  if (!f && v[i] != 0 && (n % v[i] == 0)) f = bactraking(n/v[i], i-1);
+  if (!f) f = bactraking(n -v[i], i-1);
+  if (!f) f = bactraking(n + v[i], i-1);
+  return f;
+}
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  while (true) {
+    for (int i=0; i < 5; ++i) 
+      cin >> v[i];
+    if (
+        v[0] == 0 &&
+        v[1] == 0 &&
+        v[2] == 0 &&
+        v[3] == 0 &&
+        v[4] == 0
+       ) break;
+    bool f = false;
+    do {
+      f = bactraking(23, 4);
+    } while (next_permutation(v.begin(), v.end()) && !f);
+    if (f) cout << "Possible\n";
+    else cout << "Impossible\n";
+  }
+  return 0;
+}
+
+
