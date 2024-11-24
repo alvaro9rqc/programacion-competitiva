841b7c8ddf64cbece664e3223d995b605386de5c
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-11-24 17:18:19 -0500

    Oh, it was so hard, 12878 AC

--- uva/12878-Flowery-Trails.cpp
+++ uva/12878-Flowery-Trails.cpp
@@ -4,6 +4,7 @@ using ll = long long;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
+ll sum = 0;
 
 const ll INF = LLONG_MAX;
 int main () {
@@ -15,34 +16,46 @@ int main () {
     for (ll i = 0; i < T; ++i) {
       int p1, p2, l;
       cin >> p1 >> p2 >> l;
+      if (p1 == p2) continue;
       g[p1].emplace_back(p2, l);
       g[p2].emplace_back(p1, l);
     }
     vector<ll> dist(P, INF);
-    vector<ll> acum(P, 0);
+    vector<ll> edges(P, 0);
+    vector<vi> origin(P, vi());
+    sum = 0;
     dist[0] = 0;
-    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>>pq;
-    pq.emplace(0, 0);
+    priority_queue<tuple<ll, ll,ll,ll>, vector<tuple<ll, ll,ll,ll>>, greater<tuple<ll,ll, ll,ll>>>pq;
+    pq.emplace(0, 0, 0,0);
     while(!pq.empty()) {
-      auto& [d, u, ac] = pq.top(); pq.pop();
+      auto [d, u, e, o] = pq.top(); pq.pop();
       if (d > dist[u]) continue;
-      //if (d == dist[u]) acum[u]+= e;
-      set<int> s;
-      for(auto& [w, v]: g[u]) {
-        if (w + dist[u] == dist[v]) 
-        {
-          if (s.find(v) == s.end()) {
-            acum[v] += acum[u];
-            s.insert(v);
-          }
-          acum[v] += w;
-        }
-        if (w + dist[u] >= dist[v]) continue;
+      edges[u] += e;
+      origin[u].push_back(o);
+      if (origin[u].size() != 1) continue;
+      for(auto& [v, w]: g[u]) {
+        //cout << v << ' ' << w <<" : " << u << '\n';
+        if (w + dist[u] > dist[v]) continue;
         dist[v] = w + dist[u];
-        acum[v] = w + acum[u];
-        pq.emplace(w, u, b);
+        pq.emplace(dist[v], v, w, u);
       }
     }
+    //cout <<"--> "<< dist[P-1]<<'\n';
+    set<int> vis;
+    queue<int> path;
+    path.push(P-1);
+    vis.insert(P-1);
+    while (! path.empty()) {
+      int v = path.front(); path.pop();
+      //cout << "e: " << v << " w: " << edges[v]<<'\n';
+      sum += edges[v];
+      for(auto& u: origin[v]) {
+        if (vis.find(u) != vis.end()) continue;
+        vis.insert(u);
+        path.push(u);
+      }
+    }
+    cout << 2 * sum << '\n';
   }
   return 0;
 }
