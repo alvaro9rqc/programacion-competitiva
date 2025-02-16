c8f04ece3130818ca39cce1b3802fb4256e3390f
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-02-16 17:16:48 -0500

    I learned the python recurtion limit

---
+++ atcoder/3/d.py
@@ -0,0 +1,26 @@
+import sys
+N, K = map(int,input().split())
+sys.setrecursionlimit(3 * 10**5)
+g = [ [] for _ in range(N) ]
+chos = [False] * N
+for _ in range(N-1):
+    a,b = map(int,input().split())
+    a-=1
+    b-=1
+    g[a].append(b)
+    g[b].append(a)
+ori = 0
+for i in map(int, input().split()):
+    chos[i-1] = True
+    ori = i-1
+vis = [False] * N
+def dfs( v ):
+    vis[v] = True
+    nodes = 0
+    for u in g[v]:
+        if not vis[u]:
+            nodes += dfs( u )
+    if nodes != 0 or chos[v]: nodes +=1
+    return nodes 
+print(dfs(ori))
+
