a2c8588b8c76d4765f160ac688b8b3043e45c801
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-03-19 21:53:55 -0500

    Ok, it's so late

--- codeforces/contest/r10/04/c.cpp
+++ codeforces/contest/r10/04/c.cpp
@@ -16,42 +16,22 @@ typedef vector<ll_t> vll_t;
 int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  ll t; cin  >> t;
+  int t; cin >> t;
   while(t--) {
-    ll n; cin >> n;
-    string sn = to_string(n);
-    cout << n <<"  "; 
-    ll idx =2;
-    ll dm = 10;
-    int si = sn.size();
-    for (int i = 1; si - i-1 >= 0; ++i) {
-      int j = si-i-1;
-      //cout <<": "<<j<<'\n';
-      int d = int( '7'- sn[j] );
-      if (sn[j] > '7') continue;
-      if (d < dm) {
-        dm = d;
-        idx = i;
-      }
+    string n;
+    cin >> n;
+    ll nl = stroll(n);
+    int ans;
+    {
+      int d = (int)( n.back() - '0' );
+      if (d < 7) d+= 10;
+      ans = d-7;
     }
-    ll s = 0;
-    for (auto i = 0; i < idx; ++i) {
-      s*=10;
-      s+= 9;
-    }
-    cout << s<<"  ";
-    int ans = 0;
-    while(true) {
-      sn = to_string(n);
-      if (sn.find("7") !=string::npos) {
-        cout << ans <<'\n';
-        break;
-      }
-      n += s;
-      ++ans;
+    for (auto i = n.end()-2; i >= n.begin(); --i) {
+      int d =(int)(*i-'0');
+      ans = min
     }
   }
-  return 0;
 }
 
 
