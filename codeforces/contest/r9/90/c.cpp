731ba757deed6adbdbe75fd7aed0e4ac6024f864
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-04-19 09:32:17 -0500

    AC

--- codeforces/contest/r9/90/c.cpp
+++ codeforces/contest/r9/90/c.cpp
@@ -2,40 +2,35 @@
 using namespace std;
 using ll = long long;
 typedef pair<int, int> ii;
+typedef tuple<int, int, int> iii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
+typedef vector<iii> viii;
+
+typedef pair<ll, ll> ll_p; 
+typedef tuple<ll, ll, ll> ll_t; 
+typedef vector<ll> vll;
+typedef vector<ll_p> vll_p;   
+typedef vector<ll_t> vll_t;
 
 int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int t; cin >> t;
-  while (t--) {
-    ll n; cin >> n;
-    ll sum=0;
-    vector<ll> r1(n);
-    vector<ll> r2(n);
-    for (ll i = 0; i < n; ++i) {
-      cin >> r1[i];
+  while(t--){
+    int n; cin >> n;
+    int a = int( -1e5 +5);
+    vii v(n);
+    for(auto& [x,y]: v) cin >> x;
+    for(auto& [x,y]: v) cin >> y;
+    ll ans = 0;
+    for(auto& [x,y]: v) {
+      //cout << x <<' '<<y<<'\n';
+      ans += max(x,y);
+      if (min(x,y) > a) a = min(x,y);
     }
-    for (ll i = 0; i < n; ++i) {
-      cin >> r2[i];
-    }
-    ll e=0;
-    ll esum = -2000000;
-    for (ll i = 0; i < n; ++i) {
-      if (r2[i]<r1[i]) {
-        sum+=r1[i];
-      }else sum += r2[i];
-      if (esum < (r2[i]+r1[i])) {
-        esum = (r2[i]+r1[i]);
-        e = i;
-      }
-    }
-    if (r2[e]<r1[e]) sum+=r2[e];
-    else sum += r1[e];
-    cout << sum <<'\n';
+    cout << ans + a << '\n';
   }
-  return 0;
 }
 
 
