6569925f7460078020befdbbcaa10275cb334d12
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-04-27 11:08:27 -0500

    AC

--- codeforces/contest/r10/19/c.cpp
+++ codeforces/contest/r10/19/c.cpp
@@ -1,63 +1,64 @@
 #include <bits/stdc++.h> 
 using namespace std;
 using ll = long long;
-  typedef pair<int, int> ii;
-  typedef tuple<int, int, int> iii;
-  typedef vector<int> vi;
-  typedef vector<ii> vii;
-  typedef vector<iii> viii;
+typedef pair<int, int> ii;
+typedef tuple<int, int, int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
 
-  typedef pair<ll, ll> ll_p; 
-  typedef tuple<ll, ll, ll> ll_t; 
-  typedef vector<ll> vll;
-  typedef vector<ll_p> vll_p;   
-  typedef vector<ll_t> vll_t;
+typedef pair<ll, ll> ll_p; 
+typedef tuple<ll, ll, ll> ll_t; 
+typedef vector<ll> vll;
+typedef vector<ll_p> vll_p;   
+typedef vector<ll_t> vll_t;
 
-  int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t; cin >> t;
-    while(t--) {
-      ll n, k;
-      cin >> n >> k;
-      vll a(n);
-      for(auto& e: a) cin >> e;
-      vll pos;
-      for (ll i = 0; i < n; ++i) {
-        if (a[i] <= k) pos.push_back(i);
-      }
-      auto peq = [&](ll l, ll r, ll &pr, ll& pl) {
-        ll it = ll( lower_bound(pos.begin(), pos.end(), l)-pos.begin() );
-        for(auto i = it; i < ll(pos.size()) and pos[i] <= r; ++i){
-          ll mid = pos[i] - l + 1;
-          mid = mid / 2 + ( (mid&1)?1:0 );
-          if (i+1 >= mid) {
-            pr=l;
-            pl =pos[i];
-            return true;
-          }
-        }
-        it = ll(upper_bound(pos.begin(), pos.end(), r) - pos.begin());
-        --it;
-        for (auto i = it; i >= 0 and pos[i]>=l; --i) {
-          ll mid = r-pos[i] +1;
-          mid = mid / 2 + ( (mid&1)?1:0 );
-          if (ll(pos.size()) - i >= mid) {
-            pl = pos[i];
-            pr = r;
-            return true;
-          }
-        }
-        return false;
-      };
-      ll r,l;
-      if (peq(0, n-1, l,r)) {
-        ll nr, nl;
-        if (l == 0) nl=r+1, nr=n-1;
-        else nl=0, nr=l-1;
-        if (peq(nl,nr,l,r)) cout << "yes\n";
-        else cout << "no\n";
-      } else cout << "no\n";
+
+template<typename IT>
+IT f(IT b, IT e) {
+  int ans = *b;++b;
+  for ( ; b != e; ++b) {
+    if (ans >= 0 and *b > 0) break;
+    if (ans == 0) break;
+    ans += *b;
+  }
+  if (ans >= 0) return b-1;
+  return b;
+}
+
+bool f2(vi& a) {
+  auto p = f(a.begin(), a.end()-2);
+  if (p < a.end()-2) {
+    auto pm = f(p+1, a.end()-1);
+    if (pm < a.end()-1) return true;
+    else {
+      auto pr = reverse_iterator(p);
+      pr-=2;
+      auto c = f(a.rbegin(), pr);
+      if (c < pr) return true;
+    }
+  }
+  return false;
+}
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll tt; cin >> tt;
+  while(tt--){
+    ll n, k; cin >> n >> k;
+    vi a(n);
+    for(auto& e: a) {
+      ll x; cin >> x;
+      e = x <= k ? 1:-1;
+    }
+    if (f2(a)) cout << "yes\n";
+    else {
+      //cerr << "second ";
+      reverse(a.begin(), a.end());
+      if (f2(a)) cout << "yes\n";
+      else cout << "no\n";
+    }
 
   }
 }
