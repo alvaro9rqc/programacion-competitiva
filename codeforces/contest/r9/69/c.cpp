0e452491d726910e461bb662726e01db37092fee
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-05-18 19:10:57 -0500

    Solved

--- codeforces/contest/r9/69/c.cpp
+++ codeforces/contest/r9/69/c.cpp
@@ -21,23 +21,15 @@ int main () {
     ll n,a,b ; cin >> n>>a >> b;
     ll m = gcd(a,b);
     vl A(n);
-    for(auto& e: A) cin >> e, e %= m;
-    sort(A.begin(), A.end());
-    auto it = unique(A.begin(), A.end());
-    ll mid = ll(it - A.begin());
-    if (mid & 1) mid /= 2;
-    else mid /=2, mid-=1;
-    ll ans = *(it-1) - A.front();
-    //cout << "gcd: "  << m << '\n';
-    //for(auto& e: A) cout << e << ' ';
-    //cout << '\n';
-    ll x1 = A.front() + m;
-    ll x2 = A[mid]+ m;
-    ans = min( ans, x1 - A[mid] );
-    ans = min( ans, x2 - *(it-1) );
-    ans = min(ans, max(A.front()+m-A[mid], m - ans) );
-    ans = min(ans, max(x2-x1, ll(abs(x2  - *(it-1)))));
-    //if (ans > m / 2) ans = min(ans, A.front()+m-A[mid] );
+    set<ll> s;
+    for(auto& e: A) cin >> e, e %= m, s.insert(e);
+    ll ans = *(--s.end()) - *s.begin();
+    for (auto i = 0u; i < s.size(); ++i) {
+      auto c = *s.begin();
+      s.erase(s.begin());
+      s.insert(c+m);
+      ans = min(ans, *(--s.end()) - *s.begin());
+    }
     cout << ans << '\n';
   }
 }
