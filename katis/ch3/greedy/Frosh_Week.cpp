677f710eae0d23e93d8a92e794d58550dbc0c156
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-08-18 06:15:39 -0500

    AC

---
+++ katis/ch3/greedy/Frosh_Week.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+#define endl '\n'
+#define dbg(x) cerr << #x << " = " << (x) << endl;
+#define raya cerr << " ==================== " << endl;
+#define rep(i, a, b) for (auto i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int n,m; cin >> n >> m;
+  vi t(n), l(m);
+  for(auto& e: t) cin >> e;
+  for(auto& e: l) cin >> e;
+  sort(all(t));
+  sort(all(l));
+  int idx = 0;
+  for(auto& lo: l) {
+    if (lo >= t[idx]) ++idx;
+    if (idx == n) break;
+  }
+  cout << idx << '\n';
+}
+
+
