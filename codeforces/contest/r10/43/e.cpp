1f1bc94f80462055639fbadd75c23a783b71a2c2
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-09-01 22:04:26 -0500

    AC

--- codeforces/contest/r10/43/e.cpp
+++ codeforces/contest/r10/43/e.cpp
@@ -13,46 +13,38 @@ using vl = vector<ll>;
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int tt ; cin >> tt;
+  int tt; cin >> tt;
   while(tt--) {
-    int n, m, q; cin >> n >> m >> q;
+    int n,m, q; cin >> n>>m >> q;
     vl arr(n), brr(m);
+    map<ll,int> rep;
     for(auto& i: arr) cin >> i;
     for(auto& i: brr) cin >> i;
     sort(all(arr));
-    sort(all(brr), greater<int>());
-    vl psm(n+m), joi(n+m), id(n+m);
-    iota(all(id), 0ll);
-    psm[0] = joi[0] = arr[0];
-    for (auto i = 1; i < n; i++) psm[i]= arr[i]+psm[i-1], joi[i]=arr[i];
-    for (auto i = n; i < m+n; i++) psm[i]= brr[i-n]+psm[i-1], joi[i]=brr[i-n];
-    joi.emplace_back(0);
-    id.emplace_back(0);
-    auto sum = [&psm](ll i, ll j) {
-      if (j < i) return 0ll;
-      return (psm[j]-(i==0?0ll:psm[i-1]));
-    };
+    sort(all(brr));
+    for(auto& i: arr) rep[i]++;
+    vl mer(n+m);
+    vector<ii> cnt(n+m);
+    merge(all(arr), all(brr), mer.begin());
+    for (auto i = n+m-1, x=0, y=0; i >= 0; i--) {
+      if (rep[mer[i]]) {
+        cnt[i] = {++x, y};
+        rep[mer[i]]--;
+      } else cnt[i] = {x, ++y};
+    }
+    arr.emplace_back(0);
+    brr.emplace_back(0);
+    //for(auto& [a,b]: cnt) cout << a <<' ' << b <<'\n';
+    for (auto i = n-2; i >=0; i--) arr[i]+=arr[i+1];
+    for (auto i = m-2; i >=0; i--) brr[i]+=brr[i+1];
     for (auto q_ = 0; q_ < q; q_++) {
-      ll x, y, z; cin >> x >> y >> z;
-      if (z == 0) {cout << 0 <<'\n'; continue;}
-      auto it = id.begin()+n-x;
-      auto it2 = id.begin()+n+y-z+1;
-      dbg(*it);
-      dbg(*it2);
-      //auto it2 = min(id.begin()+n+y, id.begin()+n+y-z+1);
-      auto it3 = partition_point(it, it2, [&joi, &z](int i) {
-          return joi[i+z]-joi[i]>=0;
-          });
-      ll d1 = *it3;
-      ll d2 = d1+z-1;
-      cout << (joi[1+1]-joi[1]>=0)<<'\n';
-      if (it3 == it2)--d1, --d2;
-      ll ans =sum(d1,d2);
-      dbg(d1);
-      dbg(d2);
-      //if (d2-n+1<y)ans=max(ans, sum(d1+1,d2+1));
-      //if (n - d1 < x) ans = max(ans, sum(d1-1, d2-1));
-      cout << ans <<'\n';
+      int x, y, z; cin>>x>>y>>z;
+      if (z == 0) {cout << 0 << '\n'; continue;}
+      auto [cx,cy] = cnt[n+m-z];
+      if (cx>x) {
+        cout << arr[n-x]+brr[m-z+x]<<'\n';
+      } else if (cy > y) { cout << brr[m-y]+arr[n-(z-y)]<<'\n'; }
+      else cout << arr[n-cx]+brr[m-cy]<<'\n';
     }
   }
 }
