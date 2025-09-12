da923a3f8c1e9c1d64db552eb04824cf82537f91
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-09-12 18:11:38 -0500

    C

--- uva/ch4/12768_Inspired_Procrastination.cpp
+++ uva/ch4/12768_Inspired_Procrastination.cpp
@@ -1,53 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ii = pair<int, int>;
-using vi = vector<int>;
-using vl = vector<ll>;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
 #define dbg(x) cerr << #x << " = " << (x) << endl;
 #define raya cerr << " ==================== " << endl;
-#define rep(i, a, b) for (auto i = a; i < (b); ++i)
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define sz(x) (int)(x).size()
-#define all(x) begin(x), end(x)
+#define all(x) (x).begin(), (x).end()
 
-
-using vil = vector<pair<int, ll>>;
+typedef vector<pair<int, ll> > vil;
 ll inf = ll(1e6);
 
 int main() {
-  cin.tie(0)->sync_with_stdio(0);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   cin.exceptions(cin.failbit);
+
   int n, m;
-  while(cin >> n >> m) {
-    if (n == 0 and m == 0) break;
+  while (cin >> n >> m) {
+    if (n == 0 && m == 0) break;
     vector<vil> adj(n);
-    for (auto i = 0; i < m; i++) {
-      int u, v; ll w; cin >>u>>v >>w;
-      w=-w;
-      --u,--v;
-      adj[u].emplace_back(v, w);
+    for (int i = 0; i < m; i++) {
+      int u, v; ll w; 
+      cin >> u >> v >> w;
+      w = -w;
+      --u; --v;
+      adj[u].push_back(make_pair(v, w));
     }
     vl dis(n, inf);
     dis[0] = 0;
-    for (auto i = 0; i < n; i++) {
-      bool mod = 0;
-      for (auto u = 0; u < n; u++) 
-        if (dis[u]!=inf) {
-          for(auto& [v,w]: adj[u]) {
-            ll d = dis[u]+w;
+    for (int i = 0; i < n-1; i++) {
+      bool mod = false;
+      for (int u = 0; u < n; u++) {
+        if (dis[u] != inf) {
+          for (size_t j = 0; j < adj[u].size(); j++) {
+            int v = adj[u][j].first;
+            ll w = adj[u][j].second;
+            ll d = dis[u] + w;
             if (d < dis[v]) {
-              dis[v] = (i < n-1?d:-inf);
-              mod = 1;
+              //dis[v] = (i < n - 1 ? d : -inf);
+              dis[v] = d;
+              mod = true;
             }
           }
         }
-      if (not mod) break;
+      }
+      if (!mod) break;
     }
+    bool neg = 0;
+    for (auto u = 0; u < n; u++) 
+      if (dis[u]!=inf)
+        for (size_t j = 0; j < adj[u].size(); j++) {
+          int v = adj[u][j].first;
+          ll w = adj[u][j].second;
+          ll d = dis[u] + w;
+          if (d < dis[v]) {
+            //dis[v] = (i < n - 1 ? d : -inf);
+            neg = true;
+          }
+        }
     ll ans = inf;
-    for(auto& d: dis) ans = min(ans, d);
-    if (ans == -inf) cout << "Unlimited!";
-    else cout << -ans;
-    cout <<'\n';
+    for (size_t i = 0; i < dis.size(); i++) {
+      ans = min(ans, dis[i]);
+    }
+    if (neg) cout << "Unlimited!";
+    else cout << -ans ;
+    cout << '\n';
   }
 }
 
