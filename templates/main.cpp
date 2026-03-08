d3a90c00ff7bd47f7291c1dea2ae8e1de55c4ff9
alvaro9rqc <alvaro9rqc@gmail.com>
2026-03-07 21:00:21 -0500

    feat: add submatrix to the template

--- templates/main.cpp
+++ templates/main.cpp
@@ -254,4 +254,19 @@ struct FT {
   }
 };
 
+// TODO: fix the rep
+template<class T>
+struct SubMatrix {
+  vector<vector<T>> p;
+  SubMatrix(vector<vector<T>>& v) {
+    int R = sz(v), C = sz(v[0]);
+    p.assign(R+1, vector<T>(C+1));
+    for (auto r = 0; r < R; r++) 
+      for (auto c = 0; c < C; c++) 
+        p[r+1][c+1] = v[r][c] + p[r][c+1] + p[r+1][c] - p[r][c];
+  }
+  T sum(int u, int l, int d, int r) {
+    return p[d][r] - p[d][l] - p[u][r] + p[u][l];
+  }
+};
 
