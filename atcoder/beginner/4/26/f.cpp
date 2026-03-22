03a135f2012519de873e3bc949e783181d4181e9
alvaro9rqc <alvaro9rqc@gmail.com>
2026-03-22 18:46:25 -0500

    AC

--- atcoder/beginner/4/26/f.cpp
+++ atcoder/beginner/4/26/f.cpp
@@ -10,125 +10,102 @@ using vl = vector<ll>;
 #define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
 
-const ll inf = 1e16;
+const ll inf = 1e18;
 struct Node {
   Node *l = 0, *r = 0;
-  ll lo, hi, ladd = 0, valsum = 0, valmin = inf;
-  Node(ll _lo, ll _hi) : lo(_lo), hi(_hi) {} // Large interval of −in f
-  Node(vl& v, ll _lo, ll _hi) : lo(_lo), hi(_hi) {
-    if (lo + 1 < hi) {
-      ll mid = lo + (hi - lo) / 2;
-      l = new Node(v, lo, mid);
-      r = new Node(v, mid, hi);
-      valmin = min(l->valmin, r->valmin);
-      valsum = l->valsum+r->valsum;
+  int lo, hi, vzer=0;
+  ll l_udt=0, vmin=inf;
+  Node(int _lo, int _hi) : lo(_lo), hi(_hi) {} // Large interval of −in f
+  Node(vl& v, int _lo, int _hi) : lo(_lo), hi(_hi) {
+    if (lo+1<hi){
+      int mid = lo+(hi-lo) / 2;
+      l = new Node(v,lo,mid);
+      r = new Node(v,mid,hi);
+      vmin=min(l->vmin, r->vmin);
     } else {
-      valsum = v[lo];
-      valmin = v[lo];
+      vmin=v[lo];
     }
   }
-  ll querymin(ll L, ll R) {
-    if (R <= lo || hi <= L) return inf;
-    if (L <= lo && hi <= R) return valmin;
+  ll f_mi(int L, int R) {
+    if(R<= lo or hi <= L) return inf;
+    if (L <= lo and hi <= R) return vmin;
     push();
-    return min(l->querymin(L, R), r->querymin(L, R));
+    return min(l->f_mi(L,R), r->f_mi(L,R));
   }
-  ll querysum(ll L, ll R) {
-    //cerr<<format("-> L:{} R:{}, lo {}, hi {} sum{} min{}\n", L,R,lo,hi,valsum,valmin);
-    //cerr<<"-> "<<a+b<<'\n';
-    if (R <= lo || hi <= L) return 0;
-    if (L <= lo && hi <= R) {
-      cerr<<format("qsum L:{} R:{}, lo {}, hi {} sum{} min{}\n", L,R,lo,hi,valsum,valmin);
-      return valsum;
-    }
-    push();
-    auto a = l->querysum(L,R);
-    if (a>=inf)a=0;
-    auto b = r->querysum(L,R);
-    if (b>=inf)b=0;
-    //cerr<<format("ans L:{} R:{}, lo {}, hi {} sum{} min{} a: {} b: {}\n", L,R,lo,hi,valsum,valmin,a,b);
-    return a+b;
-  }
-  //void set(int L, int R, int x) {
-  //  if (R <= lo || hi <= L) return;
-  //  if (L <= lo && hi <= R) {
-  //    mset = val = x;
-  //    madd = 0;
-  //  } else {
-  //    push();
-  //    l->set(L, R, x);
-  //    r->set(L, R, x);
-  //    val = max(l->val, r->val);
-  //  }
-  //}
-  void add(ll L, ll R, ll k) {
+  void update(int L, int R, ll k) {
     if (R <= lo || hi <= L) return;
     if (L <= lo && hi <= R) {
-      //auto m = this->querymin(L,R);
-      if (valmin == inf) return;
-      if(lo+1==hi){
-        ladd=0;
-        valsum=max(valsum-k,0ll);
-        valmin = (valmin-k>0?valmin-k:inf);
-        cerr<<format("ones add L:{} R:{}, lo {}, hi {} sum{} min{} k: {}\n", L,R,lo,hi,valsum,valmin, k);
+      if (vmin>k) {
+        vmin-=k;
+        l_udt+=k;
         return;
       }
-      auto lm = l->valmin;
-      auto rm = r->valmin;
-      if (k < lm and k < rm and lm != inf and rm != inf){
-        ladd+=k;
-        valmin-=k;
-        valsum-=k*(hi-lo);
-      } else {
+      if(lo+1<hi) {
+        l_udt+=k;
         push();
-        l->add(L,R,k);
-        r->add(L,R,k);
-        valmin=min(l->valmin, r->valmin);
-        valsum=l->valsum + r->valsum;
+        vmin=min(l->vmin, r->vmin);
+        vzer = l->vzer+r->vzer;
+      } else {
+        vzer=1;
+        vmin=inf;
+        l_udt=0;
       }
     } else {
       push();
-      l->add(L, R, k);
-      r->add(L, R, k);
-      valsum = l->valsum+r->valsum;
-      valmin = min(l->valmin, r->valmin);
+      l->update(L,R,k);
+      r->update(L,R,k);
+      vmin=min(l->vmin,r->vmin);
+      vzer=l->vzer+r->vzer;
     }
-    //cerr<<format("L:{} R:{} sum{} min{}\n", L,R,valsum,valmin);
-    cerr<<format("add L:{} R:{}, lo {}, hi {} sum{} min{} k: {}\n", L,R,lo,hi,valsum,valmin,k);
   }
   void push() {
     if (!l) {
-      ll mid = lo + (hi - lo) / 2;
+      int mid = lo + (hi - lo) / 2;
       l = new Node(lo, mid);
       r = new Node(mid, hi);
     }
-    if (ladd!=0) {
-      l->add(lo,hi,ladd);
-      r->add(lo,hi,ladd);
-      ladd=0;
+    if (l_udt) {
+      l->update(lo,hi, l_udt);
+      r->update(lo,hi, l_udt);
+      l_udt=0;
     }
+  }
+  ll f_z(int L, int R) {
+    if (R <= lo || hi <= L) return 0;
+    if (L <= lo && hi <= R) return vzer;
+    push();
+    // int mid = lo+(hi-lo)/2;
+    return l->f_z(L,R) + r->f_z(L,R);
+  }
+  ll query(int L, int R, ll k) {
+    ll ans = 0;
+    while (k) {
+      auto vm = f_mi(L,R);
+      ll d = min(k,vm);
+      k-=d;
+      ans+= d*((R-L)-f_z(L,R));
+      // dbg(f_z(L,R));
+      // dbg(d);
+      // dbg(vm);
+      update(L,R,d);
+    }
+    return ans;
   }
 };
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int n; cin >> n;
-  vl arr(n); for(auto& i: arr) cin >> i;
-  Node lsg(arr, 0, n);
-  int q; cin >> q;
-  for (auto i = 0; i < q; i++) {
-    int l, r, k; cin>> l >> r >> k;
+  int n;cin>>n;
+  vl val(n);
+  for(auto& i: val) cin >> i;
+  Node lst(val, 0, n);
+  int q;cin>>q;
+  while(q--) {
+    int l, r;ll k;cin>>l>>r>>k;
     --l;
-    auto s1 = lsg.querysum(l,r);
-    //cerr<<"\n\n";
-    lsg.add(l,r,k);
-    //cerr<<"\n\n";
-    auto s2 = lsg.querysum(l,r);
-    //dbg(s1);
-    //dbg(s2);
-    cout << (s1 - s2) <<'\n';
-    //raya;
+    cout<<lst.query(l,r,k)<<'\n';
+    // raya;
   }
 }
 
