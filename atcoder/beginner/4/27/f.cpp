deb7d19bcee48d72663cd99c651c3a9925029f1d
alvaro9rqc <alvaro9rqc@gmail.com>
2026-05-13 17:17:35 -0500

    AC

--- atcoder/beginner/4/27/f.cpp
+++ atcoder/beginner/4/27/f.cpp
@@ -12,69 +12,62 @@ using vl = vector<ll>;
 #define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
 
+
+#include <bits/extc++.h>
 // To use most b i t s rather than j u s t the lowest ones :
 struct chash { // large odd number for C
-  uint64_t C = ll(4e18 * acos(0)) | 71+ chrono::steady_clock::now().time_since_epoch().count();
+  uint64_t C = ll(4e18 * acos(0)) | 71;
   ll operator()(ll x) const { return __builtin_bswap64(x*C); }
 };
 // __gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
 using h_m = __gnu_pbds::gp_hash_table<ll,ll,chash>;
 
-int n; 
-ll m;
-vector<h_m> memo;
-vl val;
-
-ll dp(ll s, int i) {
-  if(i>=n) return s==0;
-  if(memo[i].find(s) != memo[i].end()) return memo[i][s];
-  ll ans=0;
-  ans+=dp(s,i+1);
-  ans+=dp((s+val[i])%m,i+2);
-  return memo[i][s]=ans;
-}
-
-pair<h_m, h_m> dp2(vl& v) {
-  h_m pen, ult;
-  pen[0]=1;
-  ult[0]=1;
-  ult[v[0]]+=1;
-  for (auto i = 1; i < sz(v); i++) {
-    h_m now=ult;
-    for(auto& [s,r]: pen) 
-    now[(s+v[i])%m]+=r;
-    swap(pen,ult);
-    ult=now;
+void solve() {
+  ll m;
+  auto f=[&](vl& v)->pair<h_m,h_m> {
+    int n= sz(v);
+    h_m p2,p1;
+    p2[0]=p1[0]=1;
+    for (auto i = 0; i < n; i++) {
+      vector<pair<ll,ll>> cp;
+      for(auto& [s,r]: p2) cp.emplace_back((s+v[i])%m,r);
+      p2=p1;
+      for(auto& [s,r]: cp) p1[s]+=r;
+    }
+    return {p2,p1};
+  };
+  ll n;cin>>n>>m;
+  if(n<10) {
+    vl val(n); for(auto& v: val) cin>>v;
+    auto[p2,p1]=f(val);
+    cout<<p1[0]<<'\n';
+    return ;
   }
-  return {pen,ult};
+  ll lim = n/2;
+  vl v1(lim),v2(n-lim-1);
+  ll xd;
+  for(auto& i: v1) cin >> i;
+  cin>>xd;
+  for(auto& i: v2) cin >> i;
+  reverse(all(v2));
+  auto [p2,p1]=f(v1);
+  auto [s2,s1]=f(v2);
+  ll ans = 0;
+  for(auto& [s,r]: p1) {
+    auto it = s1.find((m-s)%m);
+    if(it!=s1.end()) ans+=(r*it->second);
+  }
+  for(auto& [s,r]: p2) {
+    auto it = s2.find( (m-(xd+s)%m)%m);
+    if(it!=s2.end()) ans+=(r*it->second);
+  }
+  cout<<ans<<'\n';
 }
+
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  cin>>n>>m;
-  val.resize(n);
-  for(auto& i: val) cin>>i;
-  if(n<10) {
-    memo.assign(n,h_m());
-    cout<<dp(0,0)<<'\n';
-    return 0;
-  }
-  vl L(n/2), R(n-n/2);
-  for (auto i = 0; i < n/2; i++) L[i]=val[i];
-  for (auto i = n-1; i >= n-sz(R); i--) R[n-1-i]=val[i];
-  auto[pl,ul]=dp2(L);
-  auto[pr,ur]=dp2(R);
-  ll ans = 0;
-  for(auto& [s,r]: pl) 
-  if(ur.find((m-s)%m) != ur.end())
-    ans+=r*ur[(m-s)%m];
-  for(auto [s,r]: ul) {
-    if(pl.find(s) != pl.end()) r-=pl[s];
-    if(pr.find((m-s)%m) != pr.end()) 
-      ans+=r*pr[(m-s)%m];
-  }
-  cout<<ans<<'\n';
-  return 0;
+  solve();
 }
 
