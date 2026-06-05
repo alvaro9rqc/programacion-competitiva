a141c341759385aba154c4f8fc3de9b97a794aeb
alvaro9rqc <alvaro9rqc@gmail.com>
2026-06-05 08:46:04 -0500

    AC

--- codeforces/contest/r10/83/d.cpp
+++ codeforces/contest/r10/83/d.cpp
@@ -10,80 +10,82 @@ using vl = vector<ll>;
 #define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
 
-struct FT_s {
-  vector<int> s;
-  FT_s(int n) : s(n) {}
-  void update(int pos, int dif) { // a [pos] += dif
-    for (; pos < sz(s); pos |= pos + 1) s[pos] = max(dif,s[pos]);
+pair<vi,vi> fi(vi& val) {
+  stack<ii> sc,sp;
+  vi cl(sz(val),-1),pl(sz(val));
+  int n = sz(val);
+  for (auto i = 0; i < n; i++) {
+    //pl
+    while(sz(sp) and sp.top().first<val[i]) sp.pop();
+    if(sz(sp)) pl[i]=i-sp.top().second-1;
+    else pl[i]=i;
+    sp.emplace(val[i],i);
+    //cl
+    if((i and i <n-1)and val[i-1]<val[i] and val[i]>val[i+1]) {
+      while(sz(sc) and sc.top().first<val[i]) {
+        int d = i-sc.top().second-1;
+        if(d<pl[i]) cl[i]=sc.top().second;
+        sc.pop();
+      }
+      sc.emplace(val[i],i);
+    }
   }
-  int query(int pos) { // sum of values in [0 , pos)
-    int res = 0;
-    for (; pos > 0; pos &= pos - 1) 
-      res=max(res,s[pos-1]);
-    return res;
-  }
-};
+  return {cl,pl};
+}
 
-struct FT_p {
-  vector<int> s;
-  FT_p(int n) : s(n) {}
-  void update(int pos, int dif) { // a [pos] += dif
-    pos=sz(s)-pos-1;
-    for (; pos < sz(s); pos |= pos + 1) s[pos] = max(dif,s[pos]);
+vi memo;
+vi cl,pl,cr,pr;
+
+int dp(int i) {
+  if(i<0)return 0;
+  if(memo[i]!=-1)return memo[i];
+  auto& ans = memo[i];
+  ans=min(
+    pl[i]+dp(cr[i]),
+    pr[i]+dp(cl[i])
+  );
+  return ans;
+}
+
+void solve() {
+  int n;cin>>n;
+  vi val(n); for(auto& i: val) cin >> i; 
+  tie( cl,pl )=fi(val);
+  reverse(all(val));
+  tie( cr,pr )=fi(val);
+  reverse(all(cr));
+  reverse(all(pr));
+  for(auto& i: cr) if(i!=-1) i=n-i-1;
+
+  // for(auto& i: cl) cout<<i<<' ';
+  // cout<<'\n';
+  // for(auto& i: pl) cout<<i<<' ';
+  // cout<<'\n';
+  // for(auto& i: cr) cout<<i<<' ';
+  // cout<<'\n';
+  // for(auto& i: pr) cout<<i<<' ';
+  // cout<<'\n';
+
+  memo.assign(n,-1);
+  int xd=1;
+  for (auto i = 1; i < n-1; i++) 
+    if(val[i-1]<val[i]and val[i]>val[i+1]) {
+      xd=i; break;
+    }
+  for (auto i = 1; i < n-1; i++) {
+    if(val[i-1]<val[i]and val[i]>val[i+1] and val[xd]<val[i]) xd=i;
   }
-  int query(int pos) { // sum of values in [0 , pos)
-    pos=sz(s)-pos;
-    int res = 0;
-    for (; pos > 0; pos &= pos - 1) 
-      res=max(res,s[pos-1]);
-    return res;
-  }
-};
+  cout<<dp(n-1-xd)<<'\n';
+}
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int tt;cin>>tt;
+  int tt=1;
+  cin>>tt;
   while(tt--) {
-    int n;cin>>n;
-    vi val(n); for(auto& i: val) cin >> i;
-    FT_s s_inct(n+1),p_dect(n+1);
-    FT_p p_inct(n+1),s_dect(n+1);
-    vi s_inc(n+1);
-    vi p_inc(n+1);
-    vi s_dec(n+1);
-    vi p_dec(n+1);
-    int ans = n;
-    for(auto& i: val) {
-      s_inc[i]=1+s_inct.query(i);
-      s_inct.update(i,s_inc[i]);
-      s_dec[i]=1+s_dect.query(i);
-      s_dect.update(i,s_dec[i]);
-    }
-    for (auto j = n-1; j >= 0; j--) {
-      auto i = val[j];
-      p_inc[i]=1+p_inct.query(i);
-      p_inct.update(i,p_inc[i]);
-      p_dec[i]=1+p_dect.query(i);
-      p_dect.update(i,p_dec[i]);
-    }
-    for (auto i = 0; i < n; i++) {
-      dbg(i);
-      dbg(s_inc[val[i]]);
-      dbg(s_dec[val[i]]);
-      dbg(p_inc[val[i]]);
-      dbg(p_dec[val[i]]);
-    }
-    raya;
-    for (auto i = 0; i < n; i++) {
-      int lans = n;
-      auto x = val[i];
-      lans=min(lans, n - (s_inc[x] + p_inc[x]-1));
-      lans=min(lans, n - (s_dec[x] + p_dec[x]-1));
-      lans=min(lans, n - (s_dec[x] + p_inc[x]-1));
-      ans=min(lans,lans);
-    }
-    cout<<ans<<'\n';
+    solve();
   }
 }
+
 
